6da8c648c36e1842fc6953b37aa5b81da516b222
ZacharyGillmore <88333868+ZacharyGillmore@users.noreply.github.com>
2021-10-15 15:17:37 -0700

    Added zooming in and out functionality
    Added zooming in and out functionality with the mouse wheel.

--- main.lua
+++ main.lua
@@ -1,6 +1,7 @@
 --[[A Civilization game prototype built in lua curently in developement--]]
 win_w = 1000 --window width
 win_h = 740 --window hight
+scale = 1
 game_over = false
 math.randomseed(os.time())
 
@@ -30,18 +31,32 @@ function love.load()
   waterNum = 0
   tilesOnBoard = {}
 
-
   for i = 1,144 do
     randomTile = groundTileChoices[math.random(7)]
     table.insert(tilesOnBoard,i,randomTile)
   end
 end
-
+function love.wheelmoved(x, y)
+  if y > 0 then
+    scale = scale + .1
+  elseif y < 0 then
+    scale = scale - .1
+  end
+end
 
 function love.draw()
   local r_size = win_h/10
   local font = love.graphics.newFont("arial_narrow_7.ttf", 48/(80/r_size))
-
+  counter = 1
+  for j = 1,585,65 do
+    for i = 1,920,115 do
+      love.graphics.setColor(1,1,1)
+      love.graphics.draw(groundTiles[tilesOnBoard[counter]],(i+offsetX)*scale,(j+offsetY)*scale,0,1.4*scale)
+      love.graphics.draw(groundTiles[tilesOnBoard[counter]],(i+57.5+offsetX)*scale,(j+32.5+offsetY)*scale,0,1.4*scale)
+      counter = counter + 1
+    end
+  end
+  
   love.graphics.setColor(255/255,255/255,0/255)
   love.graphics.print("Health:",font,120,20,0,1.2,1.2)
   love.graphics.setColor(1,1,1)
@@ -56,14 +71,4 @@ function love.draw()
   love.graphics.print("Gold:",font,700,20,0,1.2,1.2)
   love.graphics.setColor(1,1,1)
   love.graphics.print(gold,font,820,20,0,1.2,1.2)
-
-  counter = 1
-  for j = 1,585,65 do
-    for i = 1,920,115 do
-      love.graphics.setColor(1,1,1)
-      love.graphics.draw(groundTiles[tilesOnBoard[counter]],i+offsetX,j+offsetY,0,1.4)
-      love.graphics.draw(groundTiles[tilesOnBoard[counter]],i+57.5+offsetX,j+32.5+offsetY,0,1.4)
-      counter = counter + 1
-    end
-  end
 end

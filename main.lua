da4c0083337ebd2a4a155b7a5e8f135ed86c8ceb
ZacharyGillmore <88333868+ZacharyGillmore@users.noreply.github.com>
2021-10-16 17:33:24 -0700

    Added x and y coordinates
    Added visibility of the mouses map position

--- main.lua
+++ main.lua
@@ -28,12 +28,11 @@ function love.load()
     ["water"] = love.graphics.newImage('sprites/waterTile.png'),
   }
   font = love.graphics.newFont("arial_narrow_7.ttf", 48/(80/(win_h/10)))
-
   gold = 0
   food = 0
   health = 100
-  offsetX = -120
-  offsetY = -120
+  offsetX = 0
+  offsetY = 0
   farmNum = 0
   mountainNum = 0
   waterNum = 0
@@ -56,7 +55,7 @@ end
 
 function love.update(dt)
   if love.keyboard.isDown("w") then
-    offsetY = offsetY + 7/scale
+    offsetY = offsetY - 7/scale
   end
   if love.keyboard.isDown("a") then
     offsetX = offsetX + 7/scale
@@ -65,8 +64,15 @@ function love.update(dt)
     offsetX = offsetX - 7/scale
   end
   if love.keyboard.isDown("s") then
-    offsetY = offsetY - 7/scale
+    offsetY = offsetY + 7/scale
   end
+  mouse_x,mouse_y = love.mouse.getPosition()
+  mouse_x = math.floor(((mouse_x-offsetX)/55)/scale)
+  local oy = 0
+  if mouse_x%2 == 1 then
+    oy = 30
+  end
+  mouse_y = math.floor(((win_h-mouse_y-offsetY+oy)/65)/scale)
 end
 
 function love.draw()
@@ -76,7 +82,7 @@ function love.draw()
     if v.x%2 == 0 then
       oy = 30
     end
-    local sx,sy = (v.x*55+offsetX)*scale, (v.y*65+oy+offsetY)*scale
+    local sx,sy = (v.x*55+offsetX)*scale, win_h-(v.y*65+oy+offsetY)*scale
     if sx > -165 and sx < win_w+110 and sy > -130 and sy < win_h then
       love.graphics.draw(groundTiles[v.type],sx,sy,0,1.4*scale)
     end
@@ -99,4 +105,10 @@ function love.draw()
   love.graphics.print("Gold:",font,700,20,0,1.2,1.2)
   love.graphics.setColor(1,1,1)
   love.graphics.print(gold,font,820,20,0,1.2,1.2)
+
+  love.graphics.setColor(181/255,185/255,205/255)
+  love.graphics.rectangle("fill",0,win_h-40,1000,40)
+
+  love.graphics.setColor(0,0,0)
+  love.graphics.print("x: " .. mouse_x .. " y: " .. mouse_y,font,50,win_h-40)
 end
